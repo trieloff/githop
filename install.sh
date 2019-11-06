795efb67f1dc19ed4a15cda8e8ccda28ddece6c0
Lars Trieloff <trieloff@adobe.com>
2019-11-06 08:31:15 +0000

    fix(vsonline): use pip to install httpie

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-pip install --user mdv
+pip install --user mdv tldr commitizen httpie
 pip install --user git+https://github.com/jeffkaufman/icdiff.git
-pip install --user tldr
-pip3 install --user commitizen
 sudo cp git-cz /usr/bin
 sudo chmod +x /usr/bin/git-cz
-sudo apt-get install --yes jq httpie
+sudo apt-get install --yes jq
