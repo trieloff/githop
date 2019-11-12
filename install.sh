ac31ff6a8ef6e923c39fec682d13be132df478eb
Lars Trieloff <lars@trieloff.net>
2019-11-12 12:02:58 -0800

    feat: install less

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ pip install --user mdv tldr commitizen httpie
 pip install --user git+https://github.com/jeffkaufman/icdiff.git
 sudo cp git-cz /usr/bin
 sudo chmod +x /usr/bin/git-cz
-sudo apt-get install --yes jq
+sudo apt-get install --yes jq less
